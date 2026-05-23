68cf6bfde9ff5ccfc3a10fbe63ea311e133c94ae
CrhistoferVera <cristofervera110@gmail.com>
2026-05-23 13:43:23 -0400

    test

--- src/fixtures/fixtures.module.ts
+++ src/fixtures/fixtures.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { FixturesService } from './fixtures.service';
 import { FixturesController } from './fixtures.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-
+//preuba autodeploy
 @Module({
   imports: [PrismaModule],
   controllers: [FixturesController],
